2b03f7b658b399e4cf6948a734a22553c26cc0d2
Noorabdelfatah <121310739+Noorabdelfatah@users.noreply.github.com>
2022-12-23 19:56:19 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter first number");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter last number");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("the prime numbers are:");
+            int sum = 0;
+            {
+                for (int y = a; y <= b; y++)
+                {
+                    for (int x = 1; x <= y; x++)
+                    {
+                        if (y % x == 0)
+                            sum++;
+                    }
+
+                    if (sum == 2)
+
+
+                    {
+                        Console.WriteLine(y);
+
+                        sum = 0;
+                    }
+                    else
+                    {
+                        sum = 0;
+                        
+                    }
+                    Console.ReadLine();
+
+
+
+                }
+
+
+
+
+
+
+
+
+
+            }
+        }
+
+
+
+
+    }
+}
---
+++ perfect number.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perfectNumbber
+{
+    class Program
+    {
+        static bool Isperfect(int number)
+        {
+            int sum = 0;
+            for (int z = 1; z < number; z++)
+            {
+                if (number % z == 0) sum = sum + 1;
+            }
+            if (sum == number return true;
+            return false;
+        }
+
+        static void Main(string[] args)
+
+        {
+            int x, y;
+            Console.WriteLine("Please enter the first number");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter second number");
+            y = int.Parse(Console.ReadLine());
+
+            for (int z = x; z <= y; z++)
+
+
+            {
+                if (Isperfect(z)) Console.WriteLine("{0}", z);
+            }
+            Console.ReadKey();
+        }
+
+
+ 
+      }
+
